bd0205d9daf1df0bd2e61515cd20d6962207b22b
marcia1695 <57416706+marcia1695@users.noreply.github.com>
2025-08-04 17:07:02 -0500

    Se crearon las constantes

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
-// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+const input_amigo = document.getElementById ("amigo");
+const ul_listaAmigos = document.getElementById ("listaAmigos");
+const listaAmigoSecreto = [];
+const ul_resultado = document.getElementById ("resultado");
