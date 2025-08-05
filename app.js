787642fa6e208e4de93f3c89d83a6472adc80072
Marcia Espino <marciesri@gmail.com>
2025-08-04 23:46:45 -0500

    Sortear Amigo Secreto

--- app.js
+++ app.js
@@ -8,3 +8,10 @@ listaAmigoSecreto.push(input_amigo.value);
 console.log(listaAmigoSecreto);
 ul_listaAmigos.innerHTML += `<li>${input_amigo.value}</li>`;
 }
+
+
+function sortearAmigo(){
+    const sorteo = Math.floor(Math.random()*listaAmigoSecreto.length);
+    const amigoSecreto = listaAmigoSecreto[sorteo];
+  ul_resultado.innerHTML = `<li>El amigo secreto sorteado es: ${amigoSecreto}</li>`;
+}
