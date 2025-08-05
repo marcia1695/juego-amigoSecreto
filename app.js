551772e2685ba13f52c2d0831f9be65b181ff0d2
Marcia Espino <marciesri@gmail.com>
2025-08-04 23:26:10 -0500

    Agrega Amigo Secreto

--- app.js
+++ app.js
@@ -2,3 +2,9 @@ const input_amigo = document.getElementById ("amigo");
 const ul_listaAmigos = document.getElementById ("listaAmigos");
 const listaAmigoSecreto = [];
 const ul_resultado = document.getElementById ("resultado");
+
+function agregarAmigo(){
+listaAmigoSecreto.push(input_amigo.value);
+console.log(listaAmigoSecreto);
+ul_listaAmigos.innerHTML += `<li>${input_amigo.value}</li>`;
+}
